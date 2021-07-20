9c3577274048400bbb85e96133a3c320d65a35b2
Alireza Ghaee <arasharg@gmail.com>
2021-07-21 01:51:54 +0430

    WIP

--- config/broadcasting.php
+++ config/broadcasting.php
@@ -45,6 +45,10 @@ return [
                 'curl_options' => [
                     CURLOPT_SSL_VERIFYHOST => 0,
                     CURLOPT_SSL_VERIFYPEER => 0,
+                    CURLOPT_HTTPHEADER => [
+                        "Connection: Upgrade",
+                        "Upgrade: websocket"
+                    ]
                 ]
             ],
         ],
