20a725c34731a0d2a45c39b08d33b766717c875e
Alireza Ghaee <arasharg@gmail.com>
2021-07-22 02:29:15 +0430

    WIP

--- app/Console/Commands/WatchTehranStockExchangePrice.php
+++ app/Console/Commands/WatchTehranStockExchangePrice.php
@@ -53,7 +53,7 @@ class WatchTehranStockExchangePrice extends Command
      */
     public function handle()
     {
-        $prices = TehranStockExchange::getSymbolsPrices();
+        $prices = self::getPrices();
 
         if($prices->has('index')) {
             $indexData = $prices->get('index');
