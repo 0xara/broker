699c5752db21afe7d575d7c4c8ea138b70a1019f
Alireza Ghaee <arasharg@gmail.com>
2021-08-09 22:22:27 +0430

    WIP

--- routes/api/v1/routes.php
+++ routes/api/v1/routes.php
@@ -2,4 +2,4 @@
 
 Route::resource('/alert','Api\V1\User\ApiUserAlertController');
 
-Route::post('/broadcasting/subscribe','Api\V1\User\ApiUserBroadcastController@subscribed');
+Route::post('/broadcasting/subscribed','Api\V1\User\ApiUserBroadcastController@subscribed');
